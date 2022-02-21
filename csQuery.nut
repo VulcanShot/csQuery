VulcanShot/csQuery

commit f20d39058662a4bc1e0b940a56a471d5ed53607d

Changed access modifiers of some functions
There was no point in `FindBy`, `Next` and `First` being public functions. They are now local ones.

csQuery.nut
@@ -24,17 +24,17 @@ function csQuery::Find(query) {
 
     switch (_selector) {
         case SELECTOR.CLASS:
-            output = FindBy(Entities.FindByClassname, body);
+            output = findBy(Entities.FindByClassname, body);
             break;
         case SELECTOR.TARGETNAME:
-            output = FindBy(Entities.FindByName, body);
+            output = findBy(Entities.FindByName, body);
             break;
         case SELECTOR.ALL_OR_FIRST:
             if (body == "*") {
-                output = First();
+                output = first();
                 break;
             }
-            output = Next();
+            output = next();
             break;
         default:
             throw "The parameter has an invalid selector"
@@ -43,7 +43,7 @@ function csQuery::Find(query) {
     return QueryArray(output);
 }
 
-function csQuery::FindBy(findFunction, arg) {
+local findBy = function(findFunction, arg) {
     local ents = [];
     for (local ent; ent = findFunction.call(Entities, ent, arg); )
     {
@@ -52,7 +52,7 @@ function csQuery::FindBy(findFunction, arg) {
     return ents;
 }
 
-function csQuery::Next() {
+local next = function() {
     local ents = [];
     for (local ent; ent = Entities.Next(ent); )
     {
@@ -61,6 +61,6 @@ function csQuery::Next() {
     return ents;
 }
 
-function csQuery::First() {
+local first = function() {
     return [ Entities.Next(null) ];
 }

commit 236120672a852d75bf62ea564fd02367a5655677

Fixed errors caused by f20d390

csQuery.nut
@@ -11,7 +11,29 @@ csQuery.SELECTOR <- {
     ALL_OR_FIRST = 42 // *
 }
 
-function csQuery::Find(query) {
+local findBy = function(findFunction, arg) {
+    local ents = [];
+    for (local ent; ent = findFunction.call(Entities, ent, arg); )
+    {
+        ents.push(ent);
+    }
+    return ents;
+}
+
+local next = function() {
+    local ents = [];
+    for (local ent; ent = Entities.Next(ent); )
+    {
+        ents.push(ent);
+    }
+    return ents;
+}
+
+local first = function() {
+    return [ Entities.Next(null) ];
+}
+
+function csQuery::Find(query) : (findBy, next, first) {
     if (typeof query == "instance")
         return QueryArray([query]);
 
@@ -41,26 +63,4 @@ function csQuery::Find(query) {
     }
 
     return QueryArray(output);
-}
-
-local findBy = function(findFunction, arg) {
-    local ents = [];
-    for (local ent; ent = findFunction.call(Entities, ent, arg); )
-    {
-        ents.push(ent);
-    }
-    return ents;
-}
-
-local next = function() {
-    local ents = [];
-    for (local ent; ent = Entities.Next(ent); )
-    {
-        ents.push(ent);
-    }
-    return ents;
-}
-
-local first = function() {
-    return [ Entities.Next(null) ];
 }